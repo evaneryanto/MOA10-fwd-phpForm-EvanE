de2823183f1eb6aa54f6a7ab38a4c7646ae296d2
evaneryanto <evaneryanto95@gmail.com>
2023-12-17 21:50:55 +0700

    Pembuatan form dan halaman untuk menampilkan data dari database

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <div class = "container">
+        <center><h2>Selamat Datang Di Website Kacamataku</h2></center>
+        <center><div><h3>Pilih Halaman</h3></div></center>
+    </div>
+    <div class = "my-4"></div>
+    <div class = "container">
+        <center>
+            <button type="button" class="btn btn-primary">Halaman Kacamata</button>
+            <button type="button" class="btn btn-primary">Halaman Pembeli</button>
+            <button type="button" class="btn btn-primary">Halaman Transaksi</button>
+            <button type="button" class="btn btn-primary">Halaman Detail Transaksi</button>
+        </center>
+    </div>
+</html>
